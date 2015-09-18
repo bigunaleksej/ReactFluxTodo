146b53190b40914e989e606571aa0f381b7d8432
AlexeyBigun <abigun@softserveinc.com>
2015-09-18 19:12:54 +0300

    Create initial structure and init webpack.config

---
+++ src/module.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+React.render(
+    <h1>22222222222211</h1>,
+    document.getElementById('content')
+);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    path = require('path'),
+    srcPath = path.join(__dirname, 'src');
+
+module.exports = {
+    target: 'web',
+    cache: true,
+    entry: {
+        module: path.join(srcPath, 'module.js'),
+        common: ['react', 'flux']
+    },
+    resolve: {
+        root: srcPath,
+        extensions: ['', '.js'],
+        modulesDirectories: ['node_modules', 'src']
+    },
+    output: {
+        path: path.join(__dirname, 'tmp'),
+        publicPath: '',
+        filename: '[name].js'
+    },
+
+    module: {
+        loaders: [
+            { test: /\.js?$/, exclude: /node_modules/, loader: 'babel?cacheDirectory' }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: 'src/index.html'
+        })
+    ],
+
+    debug: true,
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './tmp'
+    }
+};
